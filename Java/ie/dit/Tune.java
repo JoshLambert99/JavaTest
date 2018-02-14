2ae1f25fe5fe12cea4d05dc1f1132bd1fe6eedf1
Josh Lambert <joshl085@hotmail.com>
2018-02-14 16:20:16 +0000

    part1mk2

--- Java/ie/dit/Tune.java
+++ Java/ie/dit/Tune.java
@@ -7,4 +7,17 @@ public class Tune
 	private String title;
 	private String altTitle;
 	private String notation;
+
+	public void setInfo(int x, String title, String altTitle, String notation)
+	{
+		this.x = x;
+		this.title = title;
+		this.altTitle = altTitle;
+		this.notation = notation;
+	}
+
+	public Tune()
+	{
+
+	}
 }
